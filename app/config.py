b85909d4455b6828e644633e61b32ae90ae42d29
dima <flyvlogpl@gmail.com>
2025-07-06 20:50:03 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -14,9 +14,22 @@ class DB(BaseModel):
     host: str = os.getenv("POSTGRES_HOST")
     port: int = os.getenv("POSTGRES_PORT")
 
+    @property
+    def url(self):
+        return (
+            f"postgresql+asyncpg://{self.user}:{self.password}"
+            f"@{self.host}:{self.port}/{self.db}"
+        )
+
+
+class WeatherAPI(BaseModel):
+    api_key: str = os.getenv("OPEN_WEATHER_MAP_API_KEY")
+    base_url: str = os.getenv("OPEN_WEATHER_MAP_BASE_URL")
+
 
 class Settings(BaseSettings):
     db: DB = DB()
+    weather_api: WeatherAPI = WeatherAPI()
 
 
 settings = Settings()
